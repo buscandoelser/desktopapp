ca49eb0d76900e93bd83d7b37f9024833f521444
Cris Ramirez <ramirezcris.cpr@gmail.com>
2026-05-08 11:37:50 -0300

    feat : login mejorado esteticamente

--- src/main/java/com/gestion/App.java
+++ src/main/java/com/gestion/App.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -16,9 +17,10 @@ public class App extends Application {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Login.fxml"));
         Parent root = loader.load();
 
-        Scene scene = new Scene(root, 640, 520);
+        Scene scene = new Scene(root, 860, 560);
         scene.getStylesheets().add(getClass().getResource("/css/dark-futuristic.css").toExternalForm());
 
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/logo-buscando-ser.png")));
         primaryStage.setTitle("Sistema de Gestión de Internos");
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
